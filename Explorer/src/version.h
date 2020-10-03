b7c66b54c14aef2882fb89416b82cae04d0e616b
funap <31555185+funap@users.noreply.github.com>
2020-10-04 07:33:18 +0900

    ver.1.8.2.12

--- Explorer/src/version.h
+++ Explorer/src/version.h
@@ -25,7 +25,7 @@
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               8
 #define VERSION_REVISION            2
-#define VERSION_BUILD               11
+#define VERSION_BUILD               12
 
 #define _STR(x) #x
 #define STR(x) _STR(x)
