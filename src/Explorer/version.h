c420066525001fae383b46aa82360b54d9b7c92a
Atsushi Funakoshi <a.funakoshix@gmail.com>
2026-06-13 11:26:53 +0900

    ver.1.10.0

--- src/Explorer/version.h
+++ src/Explorer/version.h
@@ -25,9 +25,9 @@
 #pragma once
 
 #define VERSION_MAJOR               1
-#define VERSION_MINOR               9
+#define VERSION_MINOR               10
 #define VERSION_REVISION            0
-#define VERSION_BUILD               1
+#define VERSION_BUILD               0
 
 #define _STR(x) #x
 #define STR(x) _STR(x)
